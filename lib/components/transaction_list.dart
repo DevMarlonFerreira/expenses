99bb0f20345b97dc01e5e5e0958e4a1e515ff8bd
MarlonFerreira <devmarlonferreira@gmail.com>
2021-05-25 13:36:00 -0300

    first commit

---
+++ lib/components/transaction_list.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'package:expenses/models/transaction.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  final void Function(String) onRemove;
+
+  TransactionList(this.transactions, this.onRemove);
+
+  @override
+  Widget build(BuildContext context) {
+    return transactions.isEmpty
+        ? LayoutBuilder(
+            builder: (ctx, constraints) {
+              return Column(children: <Widget>[
+                SizedBox(height: constraints.maxHeight * 0.05),
+                Container(
+                  height: constraints.maxHeight * 0.1,
+                  child: Text(
+                    'Nenhuma transação cadastrada',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                ),
+                SizedBox(height: constraints.maxHeight * 0.05),
+                Container(
+                    height: constraints.maxHeight * 0.6,
+                    child: Image.asset('assets/images/waiting.png',
+                        fit: BoxFit.cover))
+              ]);
+            },
+          )
+        : ListView.builder(
+            itemCount: transactions.length,
+            itemBuilder: (ctx, index) {
+              final tr = transactions[index];
+              return Card(
+                elevation: 5,
+                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                child: ListTile(
+                  //leading: Container(decoration: BoxDecoration(shape:BoxShape.circle, color: Theme.of(context).primaryColor)),
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: FittedBox(
+                          child: Text('R\$${tr.value}'),
+                        )),
+                  ),
+                  title: Text(
+                    tr.title,
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  subtitle: Text(
+                    DateFormat('d MMM y').format(tr.date),
+                  ),
+                  trailing: MediaQuery.of(context).size.width > 400
+                      ? TextButton.icon(
+                          icon: Icon(Icons.delete),
+                          onPressed: () => onRemove(tr.id),
+                          label: Text('Excluir'),
+                          style: TextButton.styleFrom(
+                            primary: Theme.of(context).errorColor,
+                            textStyle: TextStyle(
+                                // fontSize: 20,
+                                ),
+                          ),
+                        )
+                      : IconButton(
+                          icon: Icon(Icons.delete),
+                          color: Theme.of(context).errorColor,
+                          onPressed: () => onRemove(tr.id),
+                        ),
+                ),
+              );
+            },
+            // children: transactions.map((tr) {}).toList(),
+            // children: <Widget>[
+            //   ..._transactions.map((tr) {
+            //     return Card(
+            //       child: Text(tr.title),
+            //     );
+            //   }).toList()
+            // ],
+          );
+  }
+}
+
+// return Card(
+//   child: Row(
+//     children: <Widget>[
+//       Container(
+//           margin: EdgeInsets.symmetric(
+//               horizontal: 15, vertical: 10),
+//           decoration: BoxDecoration(
+//               border: Border.all(
+//             color: Theme.of(context).primaryColor,
+//             width: 2,
+//           )),
+//           padding: EdgeInsets.all(10),
+//           child: Text('R\$ ${tr.value.toStringAsFixed(2)}',
+//               style: Theme.of(context).textTheme.headline6)),
+//       Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: <Widget>[
+//           Text(tr.title,
+//               style: TextStyle(
+//                 fontSize: 16,
+//                 fontWeight: FontWeight.bold,
+//               )),
+//           Text(DateFormat('d MMM y').format(tr.date),
+//               style: TextStyle(color: Colors.grey))
+//         ],
+//       )
+//     ],
+//   ),
+// );
